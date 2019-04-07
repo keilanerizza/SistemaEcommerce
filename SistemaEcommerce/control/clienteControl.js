c1b170946bd007098a6a4a2973a13a717745f559
Keilane <keilanerizza@gmail.com>
2019-04-07 04:11:29 -0300

    Página de cliente

--- SistemaEcommerce/control/clienteControl.js
+++ SistemaEcommerce/control/clienteControl.js
@@ -7,36 +7,12 @@ app.controller('clienteControl', function ($scope, $http) {
         $http.get(url).then(function (response) {
             $scope.clientes = response.data;
         }, function (error) {
+            $scope.clientes = [];
             console.log(error);
         });
     }
 
-    $scope.pesquisar();
-
-/*     $scope.clientes = [
-        {
-            'codigo': '1',
-            'nome': 'Carlos',
-            'cargo': 'Professor',
-            'endereco': 'Rua teste, 65, Jardim das Palmeiras',
-            'cidade': 'Uberlandia',
-            'cep': '38400-000',
-            'pais': 'Brasil',
-            'telefone': '34944423402',
-            'fax': '343434344'
-        },
-        {
-            'codigo': '2',
-            'nome': 'Martin Fowler',
-            'cargo': 'CEO',
-            'endereco': '40, street view, google',
-            'cidade': 'Miami',
-            'cep': '30111',
-            'pais': 'USA',
-            'telefone': '55100912333',
-            'fax': '232323'
-        },
-    ] */
+    $scope.pesquisar(); 
 
     $scope.novo = function () {
         $scope.cliente = {};
